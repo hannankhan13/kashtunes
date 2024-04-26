f8b283706e3360743b0067a9a26302185362089d
Mohammad Hanan <mohammad.hanan@capillarytech.com>
2024-04-27 02:20:22 +0530

    Add html-webpack-plugin config with changes in babel and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,21 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/', 
-
-  },
-  mode: 'development',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'), 
-    },
-    port: 3000,
-  },
   module: {
     rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
-    }
-    ]
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
